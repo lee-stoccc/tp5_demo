3c95c21ed85342acbdca5a9976efa24b1844586f
lee <870722422@qq.com>
2018-04-12 20:50:12 +0800

    增查数据库

---
+++ application/index/controller/Test.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/11
+ * Time: 20:35
+ */
+
+namespace app\index\controller;
+use think\Controller;
+use think\Db;
+use app\index\model;
+
+use app\index\model\Title;   // 使用model 的 Title 类
+
+class Test extends Controller{
+    //直接操作数据库
+    public function index(){
+        header('Access-Control-Allow-Origin:http://192.168.2.120:8081');
+        $B=new Db;
+        $bb=$B::table('title')->select();
+        return json_encode($bb);
+    }
+
+    //根据前端传过来的 ID 查询响应数据
+    public function search($id){
+        header('Access-Control-Allow-Origin:*');
+        $B=new Db;
+        $bb=$B::table('title')->where('id','=',$id)->find();
+        return json_encode($bb);
+    }
+
+    //使用 model 的方法，在模型中操作数据库，在控制器中实例化模型，再调用模型的函数
+    public function use_model(){
+        $use = new Title();   //第一种方法，在 use 引用
+//      $use = new \app\index\model\Title;    //第二种方法，直接new + 命名空间
+        $data = $use->select_all();    // 实例化出来的use 调用 Title 类的 title() 方法
+        return $data;    //将数据返还
+    }
+
+    //单条数据查询 ， 有参数的传入，进行选择查询
+    public function select_sigle($id){
+        header('Access-Control-Allow-Origin:*');
+        $D=new Title();
+        $data=$D->select_sigle($id);
+        return $data;
+    }
+
+    public function insect_goos($title,$price,$address,$person,$persons,$slider,$switchs,$radioGroup,$checkbox){
+        $goods=new Title();
+        $res=$goods->inset_goods($title,$price,$address,$person,$persons,$slider,$switchs,$radioGroup,$checkbox);
+        return $res;
+    }
+}
---
+++ application/index/model/Title.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/12
+ * Time: 17:08
+ */
+namespace app\index\model;
+use think\Model;
+use think\Db;
+class Title extends Model{
+    protected $connection=[
+        'type'=>'mysql',
+        'hostname'=>'127.0.0.1',
+        'database'=>'demo',
+        'username'=>'root',
+        'password'=>'123',
+        'hostport'=>'3306',
+    ];
+
+    // 查询所有数据
+    public function select_all(){
+        $title= new Db;
+        $data=$title::table('title')->select();
+        return json_encode($data);
+    }
+
+    //根据 id 查询单条数据
+    public function select_sigle($id){
+        $title =new Db;
+        $data=$title::table('title')->where('id','=',$id)->find();
+        return json_encode($data);
+    }
+
+    //客户端提交数据，insect 入数据库,$data 数组的变量名必须与数据库字段名保持一致
+    public function inset_goods($title,$price,$address,$person,$persons,$slider,$switchs,$radioGroup,$checkbox){
+        $data=['address'=>$address,'price'=>$price,'dis'=>$address,'person'=>$person,'persons'=>$persons,'title'=>$title];
+        $goods = new Db;
+        $res=$goods::table('title')->insert($data);
+        return json_encode($res);
+    }
+}
